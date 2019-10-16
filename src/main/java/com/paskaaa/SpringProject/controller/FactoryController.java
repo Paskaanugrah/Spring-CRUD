5322004f9034194b4f2ad2e7d773a5cea15c1376
Paska Anugrah A <paskaadil@gmail.com>
2019-10-16 14:04:20 +0700

    add factory crud

--- src/main/java/com/paskaaa/SpringProject/controller/FactoryController.java
+++ src/main/java/com/paskaaa/SpringProject/controller/FactoryController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.paskaaa.SpringProject.entity.Factory;
 import com.paskaaa.SpringProject.services.FactoryService;
@@ -26,5 +31,36 @@ public class FactoryController {
 		return "factory/factory";
 	}
 	
+	@RequestMapping("/new")
+	public String showNewFactoryPage(Model model) {
+		Factory factory = new Factory();
+		
+		model.addAttribute("factory", factory);
+		
+		return "factory/create";
+	}
 	
+	@PostMapping(value = "/save")
+	public String saveFactory(@ModelAttribute("factory") Factory factory) {
+		service.save(factory);
+		System.out.println("------------------------------------------------------------------");
+		return "redirect:/factory";
+	}
+	
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditPage(@PathVariable("id") Integer id) {
+		ModelAndView mav = new ModelAndView("factory/edit");
+		
+		Factory factory = service.getById(id);
+		mav.addObject("factory", factory);
+		
+		return mav;
+	}
+	
+	@RequestMapping("delete/{id}")
+	public String deleteFactory(@PathVariable("id") Integer id) {
+		service.delete(id);
+		
+		return "redirect:/factory";
+	}
 }
